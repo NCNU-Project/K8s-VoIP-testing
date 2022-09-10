b2511a8b62b5f102ae6f97a93ab1f8a56e5db6c0
jerry <efficacy38@gmail.com>
2022-09-10 21:59:59 +0900

    update

--- service-generator.py
+++ service-generator.py
@@ -3,14 +3,14 @@ template = """
 apiVersion: v1
 kind: Service
 metadata:
-  name: rtpengine-163-22-22-68-p0
+  name: rtpengine-10-22-149-230-p{partition}
   annotations:
-    metallb.universe.tf/allow-shared-ip: "key-to-share-163.22.22.68"
+    metallb.universe.tf/allow-shared-ip: "key-to-share-10.22.149.230"
 spec:
   type: LoadBalancer
   selector:
-    ip: 163.22.22.68
-    partition-leader-0: "True"
+    ip: 10.22.149.230
+    rtpengine-10.22.149.230-p{partition}: "True"
   ports:
 {port_template}\
 """
@@ -21,7 +21,11 @@ portTemplate = """\
       targetPort: {port}
 """
 
-tmp = []
-for i in range(20000, 20020):
-    tmp.append(portTemplate.format(port=i))
-print(template.format(port_template="".join(tmp)))
+base = 20000
+partition_size = 2000
+for p in range(0, 3):
+    tmp = []
+    for i in range(0, partition_size):
+        tmp.append(portTemplate.format(port=base + p * partition_size + i))
+    print(template.format(port_template="".join(tmp), partition=p))
+    print("---")
